1b0f2d96a0555a458c27bd2a2946f12bc6734aaf
Aukryx <thomaslambert9976@gmail.com>
2026-03-27 12:12:27 +0100

    init expo

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text style={styles.title}>MealCraft v2</Text>
+        <Text>Environnement prêt. Prochaine étape : SQLite !</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  }
+});
